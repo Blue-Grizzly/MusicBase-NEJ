47c6454dacb9e0434e55d8b7b2fe4f36aeff641e
Blue-Grizzly <123905990+Blue-Grizzly@users.noreply.github.com>
2023-10-09 11:33:03 +0200

    Added models for album, artist, track

---
+++ model/album.js
@@ -0,0 +1,8 @@
+export default class Album{
+    constructor(album){
+        this.id = album.id
+        this.name = album.name
+        this.image = album.image
+        this.tracks = album.tracks
+    }
+}
