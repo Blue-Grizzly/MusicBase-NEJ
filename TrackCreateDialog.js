c7fda082d4f28b5ed19dfb3bb60662ff5ad34ed4
Jasper Nielsen <jasper.nielsen@hotmail.com>
2023-10-10 15:25:43 +0200

    create-artist-dialog complete

---
+++ TrackCreateDialog.js
@@ -0,0 +1,43 @@
+import Dialog from "./dialog.js";
+import Track from "./model/track.js";
+import * as controller from  "./script.js"
+
+export default class TrackCreateDialog extends Dialog{
+
+
+    renderHTML() {
+        const html = /*HTML */`
+        <form action="" method="dialog"  id="create-track-form">
+            <label for="create-name" >Track Name:</label><input type="text" id="create-name" name="name" placeholder="The track title">
+            <label for="create-length">Track Duration: </label> <input type="text" id="create-length" name="length" placeholder="The track duration">
+            <label for="create-artist">Track Artist: </label> <input type="text" id="create-artist" name="artist" placeholder="The track artist">
+            <label for="create-album">Track album: </label> <input type="text" id="create-album" name="album" placeholder="The track album">
+
+            <button data-action="cancel">Cancel</button>
+            <button data-action="create">Create</button>   
+        </form>
+        `
+        return html;
+    }
+
+create(){
+    const form = this.dialog.querySelector("form")
+    this.Track = new Track({
+        name: form.name.value,
+        length:form.length.value,
+        artist: form.artist.value,
+        album: form.album.value
+    });
+
+    form.reset();
+
+    controller.createTrack(this.Track)
+}
+
+
+
+
+
+
+
+}
---
+++ dialog.js
@@ -0,0 +1,44 @@
+export default class Dialog {
+    constructor(id) {
+        // create dialog-element
+        this.dialog = document.createElement("dialog");
+        this.dialog.id = id;
+        // this.dialog.setAttribute("open", false)
+        document.querySelector("main").insertAdjacentElement("afterend", this.dialog);
+    }
+    
+
+    show() {
+        this.dialog.showModal();
+    }
+
+    close(){
+        this.dialog.close()
+    }
+
+    render() {
+        // get HTML from extending class
+        const html = this.renderHTML();
+        this.dialog.innerHTML = html;
+        this.postRender();
+    }
+
+
+    postRender() {
+        // Add eventlisteners to actions
+        this.dialog.querySelectorAll("[data-action]").forEach(element =>
+            element.addEventListener("click", (event) => {
+                const action = event.target.dataset.action;
+                switch (action) {
+                    case "create": this.create(); break;
+                    case "submit": this.submit(); break;
+                    case "update": this.update(); break;
+                    case "cancel": this.close(); break;
+                    default: console.error("Unknown action: " + action);
+                }
+            })
+        );
+    }
+
+
+}
